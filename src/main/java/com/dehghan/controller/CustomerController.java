7d9d860c755aab3dfad484d96e5ffa83406c4f73
Parivash Dehghan <tania.dehghan27@gmail.com>
2023-04-08 01:11:10 +0300

    Third Commit

--- src/main/java/com/dehghan/controller/CustomerController.java
+++ src/main/java/com/dehghan/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.dehghan.controller;
 
 import com.dehghan.model.CustomerEntity;
 import com.dehghan.service.CustomerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,41 +14,39 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    public CustomerController( CustomerService customerService) {
+    public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
-
 
     }
 
     @GetMapping
-    public List<CustomerEntity> getAllCustomers(){
+    public List<CustomerEntity> getAllCustomers() {
         return customerService.getAllCustomer();
 
     }
 
     @PostMapping
-    public CustomerEntity createCustomer(@RequestBody CustomerEntity newCustomer){
+    public CustomerEntity createCustomer(@RequestBody CustomerEntity newCustomer) {
         return customerService.saveOneCustomer(newCustomer);
     }
 
     @PutMapping("/{customerId}")
-    public CustomerEntity updateOneUser(@PathVariable Long customerId, @RequestBody CustomerEntity newCustomer){
-
-
+    public CustomerEntity updateOneUser(@PathVariable Long customerId, @RequestBody CustomerEntity newCustomer) {
         return customerService.updateOneCustomer(customerId, newCustomer);
 
     }
 
     @DeleteMapping("/{customerId}")
-    //Sadece Id'sını aldığı kişiyi silecek
-    public void deleteOneCustomer(@PathVariable Long customerId){
+    public void deleteOneCustomer(@PathVariable Long customerId) {
 
         customerService.deleteById(customerId);
     }
 
     @GetMapping("/search")
     public List<Map<String, Object>> searchCustomers(@RequestParam("name") String keyword) {
-        return customerService.getCustomersWithOrders(keyword);
+        List<Map<String, Object>> result = customerService.getCustomersWithOrders(keyword);
+        return result;
+
     }
 
     @GetMapping("/without-orders")
